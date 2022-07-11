6350ea7bc9ae99efa9261f74228817745f57553d
Илья Авдеев <ilia.avdey_jr@icloud.com>
2022-07-11 22:56:31 +0300

    готовый код

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,2 +1,11 @@
 public class Post {
+    public  FormDate birthday;
+    public String name; //поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для Фамилии
+    public boolean subscription; // поле для ответа
+
+
 }
